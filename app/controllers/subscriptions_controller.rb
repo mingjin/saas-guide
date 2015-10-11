66e3854ab1cc021da7edb58c5a1378e04e92d39a
Ashwin <acharya.ash@gmail.com>
2015-10-11 15:40:34 -0700

    Stripe Subscriptions Part 2

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,9 +1,26 @@
 class SubscriptionsController < ApplicationController
+    before_filter :authenticate_user!
+    
     def new
+        @plans = Plan.all
     end
 
     def create
         ap "Inside create in subscription. See params hash"
         ap params
+
+        # Get the credit card details submitted by the form
+        token = params[:stripeToken]
+
+        plan  = params[:plan][:stripe_id]
+        email = current_user.email
+
+        #Create a Customer
+        customer = Stripe::Customer.create(
+          :source => token,
+          :plan   => plan,
+          :email  => email
+        )
+
     end
 end
