ef5ce6d36ca77b9a377e640a380417cab3ff1417
Ashwin <acharya.ash@gmail.com>
2015-10-14 23:17:05 -0700

    Stripe Update Card

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -14,6 +14,35 @@ class SubscriptionsController < ApplicationController
         @account = Account.find_by_email(current_user.email)
     end
 
+    #Entry point for showing update card view (the credit card form)
+    def update_card
+    end
+
+    #Accepts the token parameter from Stripe and updates the credit card on the customer object
+    def update_card_details
+        #Take the token given by stripe and set it on customer
+        token = params[:stripeToken]
+        #Get customer_id
+        current_account = Account.find_by_email(current_user.email)
+        customer_id     = current_account.customer_id
+
+        ap customer_id
+        ap token
+
+        #Get customer from Stripe
+        customer = Stripe::Customer.retrieve(customer_id)
+        #Set new card token
+        customer.source = token
+        customer.save
+
+        redirect_to "/subscriptions", :notice => "Card updated successfully"
+
+    rescue => e
+        redirect_to :action => "update_card", :flash => { :notice => e.message }
+
+
+    end
+
     def create
         # Get the credit card details submitted by the form
         token           = params[:stripeToken]
@@ -59,72 +88,73 @@ class SubscriptionsController < ApplicationController
         redirect_to :back, :flash => {:error => e.message }
 
     end
-end
 
-def cancel_subscription
-    email           = current_user.email
-    current_account = Account.find_by_email(current_user.email)
-    customer_id     = current_account.customer_id
-    current_plan    = current_account.stripe_plan_id
 
-    if current_plan.blank?
-        raise "No plan found to unsubscribe/cancel"
+    def cancel_subscription
+        email           = current_user.email
+        current_account = Account.find_by_email(current_user.email)
+        customer_id     = current_account.customer_id
+        current_plan    = current_account.stripe_plan_id
+
+        if current_plan.blank?
+            raise "No plan found to unsubscribe/cancel"
+        end
+
+        #Fetch customer from Stripe
+        customer = Stripe::Customer.retrieve(customer_id)
+
+        #Get customer's subscriptions
+        subscriptions = customer.subscriptions
+
+        #Find the subscription that we want to cancel
+        current_subscribed_plan = subscriptions.data.find { |o| o.plan.id == current_plan }
+
+        if current_subscribed_plan.blank?
+            raise "Subscription not found!!"
+        end
+
+        #Delete it
+        current_subscribed_plan.delete
+
+        #Update account model
+        save_account_details(current_account, "", customer_id, Time.at(0).to_datetime)
+
+        @message = "Subscription cancelled successfully"
+
+    rescue => e
+        redirect_to "/subscriptions", :flash => {:error => e.message}
     end
 
-    #Fetch customer from Stripe
-    customer = Stripe::Customer.retrieve(customer_id)
-
-    #Get customer's subscriptions
-    subscriptions = customer.subscriptions
-
-    #Find the subscription that we want to cancel
-    current_subscribed_plan = subscriptions.data.find { |o| o.plan.id == current_plan }
-
-    if current_subscribed_plan.blank?
-        raise "Subscription not found!!"
+    def save_account_details(account, plan, customer_id, active_until)
+        #Update Account model
+        account.stripe_plan_id = plan
+        account.customer_id    = customer_id
+        account.active_until   = active_until
+        account.save!
     end
 
-    #Delete it
-    current_subscribed_plan.delete
+    #Doc
+    def create_or_update_subscription(customer, current_plan, new_plan)
+        subscriptions = customer.subscriptions
 
-    #Update account model
-    save_account_details(current_account, "", customer_id, Time.at(0).to_datetime)
+        #Get current subscriptions
+        current_subscription = subscriptions.data.find{ |o| o.plan.id == current_plan }
 
-    @message = "Subscription cancelled successfully"
+        if current_subscription.blank?
+            #No current subscription
+            #Maybe the customer unsubscribed previously or the card was declined
+            #So, create a new subscription to existing customer
+            subscription = customer.subscriptions.create({:plan => new_plan })
+        else
+            #Existing subscription found
+            #Must be an upgrade or a downgrade
+            #So update eistig subscription with new plan
+            current_subscription.plan = new_plan
+            subscription = current_subscription.save
+        end
 
-rescue => e
-    redirect_to "/subscriptions", :flash => {:error => e.message}
-end
+        return subscription
 
-def save_account_details(account, plan, customer_id, active_until)
-    #Update Account model
-    account.stripe_plan_id = plan
-    account.customer_id    = customer_id
-    account.active_until   = active_until
-    account.save!
-end
 
-#Doc
-def create_or_update_subscription(customer, current_plan, new_plan)
-    subscriptions = customer.subscriptions
-
-    #Get current subscriptions
-    current_subscription = subscriptions.data.find{ |o| o.plan.id == current_plan }
-
-    if current_subscription.blank?
-        #No current subscription
-        #Maybe the customer unsubscribed previously or the card was declined
-        #So, create a new subscription to existing customer
-        subscription = customer.subscriptions.create({:plan => new_plan })
-    else
-        #Existing subscription found
-        #Must be an upgrade or a downgrade
-        #So update eistig subscription with new plan
-        current_subscription.plan = new_plan
-        subscription = current_subscription.save
     end
-
-    return subscription
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   
   get "/subscriptions/cancel_subscription" => "subscriptions#cancel_subscription"
+  get "/subscriptions/update_card"          => "subscriptions#update_card"
+  post "/subscriptions/update_card_details" => "subscriptions#update_card_details"
   resources :subscriptions
 
   root to: "home#index"
