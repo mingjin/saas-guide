068ccc50b06aaa7862d210a4ace5b584465e7b64
Ashwin <acharya.ash@gmail.com>
2015-10-18 13:34:26 -0700

    Apartment - Create Tenant,Account and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
          #:async
 
   validate :email_is_unique, on: :create
+  validate :subdomain_is_unique, on: :create
+  after_validation :create_tenant
   after_create :create_account
 
   def confirmation_required?
@@ -26,9 +28,29 @@ class User < ActiveRecord::Base
     end
   end
 
+  #Subdomain should be unique
+  def subdomain_is_unique
+    if subdomain.present?
+      unless Account.find_by_subdomain(subdomain).nil?
+        errors.add(:subdomain, "is already used")
+      end
+    end
+
+    if Apartment::Elevators::Subdomain.excluded_subdomains.include?(subdomain)
+      errors.add(:subdomain, "is not a valid subdomain")
+    end
+  end
+
   def create_account
-    account = Account.new(:email => email)
+    account = Account.new(:email => email, :subdomain => subdomain)
     account.save!
+  end
+
+  def create_tenant
+    return false unless self.errors.empty?
+    
+    Apartment::Tenant.create(subdomain)
+    Apartment::Tenant.switch!(subdomain)
   end
 
 
