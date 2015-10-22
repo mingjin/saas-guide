6304c0b24260245842fb1800cc47d2acad136d8f
Ashwin <acharya.ash@gmail.com>
2015-10-21 20:56:49 -0700

    Apartment - TenantNotFound logging exception

--- lib/rescue_apartment_middleware.rb
+++ lib/rescue_apartment_middleware.rb
@@ -4,7 +4,9 @@
     begin
       #Call Apartment Middleware to do its job
       super
-    rescue Apartment::TenantNotFound
+    rescue Apartment::TenantNotFound => e
+      ap "Apartment::TenantNotFound -> Exception occured"
+      ap e.message
       return [404, {"Content-Type" => "text/html"}, ["#{File.read(Rails.root.to_s + '/public/404.html')}"] ]
 
     end
